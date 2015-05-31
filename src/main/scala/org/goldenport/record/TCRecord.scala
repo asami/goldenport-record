a234d6091481d78ad1b95d0405be5eed496f299b
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2015-05-31 16:34:41 +0900

    Adds a Record type class feature.

---
+++ src/main/scala/org/goldenport/record/TCRecord.scala
@@ -0,0 +1,11 @@
+package org.goldenport.record
+
+/*
+ * @since   May. 31, 2015
+ * @version May. 31, 2015
+ * @author  ASAMI, Tomoharu
+ */
+trait TCRecord[T] {
+  def toV2(rec: T): v2.Record
+  def toV3(rec: T): v3.Record
+}
